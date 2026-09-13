d90083380eeca38abc2aeb477099d6118fd8d8a3
Hamed F <hamedfallah7007@gmail.com>
2026-09-13 21:47:51 +0200

    V1.90: update production version for Sant Pau transport fix

--- jobbot/production.py
+++ jobbot/production.py
@@ -9,7 +9,7 @@ from typing import Callable
 from sources import academicpositions, ats_watchlist, biocat, bist, csic_institutes, euraxess, fbg_ub, fisabio, fps_andalucia, gencat_research, hospital_del_mar, idibell, iislafe, ikerbasque_calls, infojobs_spain, institutions, isciii_employment, linkedin_mads, madrid_idi_history as madrid_idi, sant_pau_research, idibaps, upc_talenthub
 
 FROZEN_ENGINE = "V1.37_TRANSFERABLE_ROLE_CALIBRATION"
-PRODUCTION_VERSION = "V1.89_MADRID_HISTORICAL_DETAIL_FALLBACK"
+PRODUCTION_VERSION = "V1.90_SANTPAU_BOUNDED_TRANSPORT"
 FREEZE_MANIFEST = "SCORING_FREEZE_V137.json"
 SOURCE_ORDER = ("linkedin", "infojobs", "academicpositions", "ikerbasque", "atswatch", "santpau", "fbg", "biocat", "gencat", "csic", "isciii", "idibaps", "upc", "idibell", "hospitaldelmar", "euraxess", "bist", "institutions", "madrid", "fisabio", "fps", "iislafe")
 SOURCE_LABELS = {
